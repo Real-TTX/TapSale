16922ed32e25356fa25ed9d57ce91a5d3d147063
Matthias Schmoldt <matthias@schmoldt.info>
2026-08-05 23:26:40 +0200

    fix: prevent font shift during reload

--- src/TabSale.Web/wwwroot/service-worker.js
+++ src/TabSale.Web/wwwroot/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='tabsale-v35';
+const CACHE='tabsale-v36';
 const SHELL=['/css/site.css','/js/site.js','/js/sale.js','/js/appearance.js','/manifest.webmanifest','/favicon.ico','/icons/app-icon.svg','/icons/app-icon-192.png','/icons/app-icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
